c0e6b42d3558f2c1928cb4740eff75e0e7ada966
PantherV <PantherV@users.noreply.github.com>
2024-06-04 10:29:04 +0700

    FIX code video 45 config project folder, controller and service

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.laptopshop.service.UserService;
+
 // @Controller
 // public class UserController {
 
@@ -16,6 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+  private UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
   @GetMapping("")
   public String getHomePage() {
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -5,11 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-  private UserService userService;
-
-  public UserService(UserService userService) {
-    this.userService = userService;
-  }
 
   public String handleHello() {
     return "Hello from service ";
