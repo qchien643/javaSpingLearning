0e473ce272171ef589fc3608e427d5203b62d688
PantherV <PantherV@users.noreply.github.com>
2024-06-06 10:21:33 +0700

    video 46

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,16 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//   @RequestMapping("/")
-//   public String getHomePage() {
-//     return "hello from controller";
-//   }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
   private UserService userService;
@@ -25,8 +16,24 @@ public class UserController {
     this.userService = userService;
   }
 
-  @GetMapping("")
+  @RequestMapping("/")
   public String getHomePage() {
-    return this.userService.handleHello();
+    String test = this.userService.handleHello();
+    return "eric.html";
   }
 }
+
+// @RestController
+// public class UserController {
+// // DI: dependency injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
