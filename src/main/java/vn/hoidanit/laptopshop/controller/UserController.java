1566f799b69a7de352d6cad0fea9443e2ffb5b37
PantherV <PantherV@users.noreply.github.com>
2024-06-12 17:18:28 +0700

    video 62-63 exercise

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -22,6 +24,8 @@ public class UserController {
 
   @RequestMapping("/")
   public String getHomePage(Model model) {
+    List<User> arrUsers = this.userService.getAllUsersByEmail("phamquangchien243@gmail.com");
+    System.out.println(arrUsers);
     model.addAttribute("eric", "test");
     model.addAttribute("hoidanit", "from controller with model");
     return "hello";
@@ -29,11 +33,16 @@ public class UserController {
 
   @RequestMapping("/admin/user")
   public String getUserPage(Model model) {
+    return "admin/user/table-user";
+  }
+
+  @RequestMapping("/admin/user/create") // GET
+  public String getCreateUserPage(Model model) {
     model.addAttribute("newUser", new User());
     return "admin/user/create";
   }
 
-  @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+  @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
   public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
     System.out.println("run here " + hoidanit);
     this.userService.handleSaveUser(hoidanit);
