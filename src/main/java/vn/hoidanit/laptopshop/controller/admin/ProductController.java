c289816316df9547ecbddbfc6f6ac6972a0ba346
PantherV <PantherV@users.noreply.github.com>
2024-08-06 16:03:42 +0700

    video 99 design giao dien create product

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -1,12 +1,49 @@
 package vn.hoidanit.laptopshop.controller.admin;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.service.ProductService;
+import vn.hoidanit.laptopshop.service.UploadService;
 
 @Controller
 public class ProductController {
+  private final ProductService productService;
+  private final UploadService uploadService;
+
+  public ProductController(ProductService productService, UploadService uploadService) {
+    this.productService = productService;
+    this.uploadService = uploadService;
+  }
+
   @GetMapping("/admin/product")
-  public String getDashboard() {
+  public String getProduct(Model model) {
+    List<Product> products = this.productService.getAllProducts();
+    model.addAttribute("product", products);
     return "admin/product/show";
+  }
+
+  @GetMapping("/admin/product/create") // GET
+  public String getCreateProductPage(Model model) {
+    model.addAttribute("newProduct", new Product());
+    return "admin/product/create";
+  }
+
+  @PostMapping(value = "/admin/product/create")
+  public String createProductPage(Model model,
+      @ModelAttribute("newProduct") Product hoidanit,
+      @RequestParam("hoidanitFile") MultipartFile file) {
+    // String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
+
+    // hoidanit.setAvatar(avatar);
+
+    // save
+    // his.userService.handleSaveProduct(hoidanit);
+    return "redirect:/admin/product";
   }
 }
---
+++ src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
@@ -0,0 +1,20 @@
+package vn.hoidanit.laptopshop.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.repository.ProductRepository;
+
+@Service
+public class ProductService {
+  private final ProductRepository productRepository;
+
+  public ProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  public List<Product> getAllProducts() {
+    return this.productRepository.findAll();
+  }
+}
