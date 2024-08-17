07404d78e5740e6902f7d7002223febe71355e9b
PantherV <PantherV@users.noreply.github.com>
2024-08-17 12:56:50 +0700

    video 122 Spring Session

--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -3,7 +3,7 @@ package vn.hoidanit.laptopshop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.hoidanit.laptopshop.domain.Product;
-import java.util.List;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
