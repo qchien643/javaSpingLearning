fa60608d7f9d5dce82403c8c20bde154b2473f77
PantherV <PantherV@users.noreply.github.com>
2024-08-17 10:49:04 +0700

    video 119 Authorize by Role

--- src/main/java/vn/hoidanit/laptopshop/service/CustomUserDetailsService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/CustomUserDetailsService.java
@@ -21,13 +21,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     // TODO logic
     vn.hoidanit.laptopshop.domain.User user = this.userService.getUserByEmail(username);
     if (user == null) {
-      throw new UsernameNotFoundException("user not found");
+      throw new UsernameNotFoundException("User not found");
     }
 
     return new User(
         user.getEmail(),
         user.getPassword(),
-        Collections.singletonList(new SimpleGrantedAuthority(user.getRole().getName())));
+        Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName())));
   }
 
 }
